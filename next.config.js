681950c5b1f59a5750bb4b54051a81948229a588
goldrusher9009-sketch <goldrusher9009@gmail.com>
2026-05-01 16:51:23 -0400

    Remove env variables from next.config.js
    Removed environment variables from next.config.js.

--- next.config.js
+++ next.config.js
@@ -5,12 +5,5 @@ const nextConfig = {
   experimental: {
     esmExternals: true,
   },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-  },
-};
 
 module.exports = nextConfig;
